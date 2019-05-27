50986a1043c6f2f69a6a7627923d9db6f1a7f207
Vicente <chente54321@gmail.com>
2019-05-26 23:42:36 -0500

    cambios

--- docs personalizados/docs.c
+++ docs personalizados/docs.c
@@ -1,41 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void leerEmpleados();
-void leerNota();
+void leerEmpleados(FILE *empleadosFile, char **empleados, int tam);
+void leerNota(FILE *nota);
+void crearNotas(char **empleados, FILE *nota, int tam);
+int contarEmpleados(FILE *empleadosFile);
 
 int main()
 {
-	leerEmpleados();
+	FILE *empleadosFile;
+	FILE *nota;
+	char **empleados;
+	int noEmpleados;
+	noEmpleados = contarEmpleados(empleadosFile);
+	empleados = (char **)malloc(noEmpleados * sizeof(char *));
+	for (int i = 0; i < noEmpleados; i++)
+    {
+        empleados[i] = (char *)malloc(50 * sizeof(char));
+    }
+	printf("numero de empleados: %d\n", noEmpleados);
+	leerEmpleados(empleadosFile, empleados, noEmpleados);
 	printf("\n");
-	leerNota();
+	leerNota(nota);
+	printf("\n");
+	crearNotas(empleados, nota, noEmpleados);
 	return 0;
 }
 
-void leerEmpleados()
+int contarEmpleados(FILE *empleadosFile)
 {
-	FILE *empleados;
 	char caracter;
-	empleados = fopen("empleados.txt", "r");
+	int contador = 0;
+	empleadosFile = fopen("empleados.txt", "r");
 
-	if (empleados == NULL)
+	if (empleadosFile == NULL)
 	{
 		printf("\nError de apertura del archivo. \n\n");
 	}
 	else
 	{
-		printf("\nEl contenido del archivo de empleados es \n");
-		while ((caracter = fgetc(empleados)) != EOF)
+		while (!feof(empleadosFile))
 		{
-			printf("%c", caracter);
+			caracter = fgetc(empleadosFile);
+			if (caracter == '\n' || caracter == '\0' || caracter == EOF)
+			{
+				contador++;
+			}
 		}
 	}
-	fclose(empleados);
+	fclose(empleadosFile);
+	return contador;
 }
 
-void leerNota()
+void leerEmpleados(FILE *empleadosFile, char **empleados, int tam)
 {
-	FILE *nota;
+
+	char nombres[100];
+	char *nombre;
+	int i = 0;
+	empleadosFile = fopen("empleados.txt", "r");
+	if (empleadosFile == NULL)
+	{
+		printf("\nError de apertura del archivo. \n\n");
+	}
+	else
+	{
+		while (!feof(empleadosFile))
+		{
+			nombre = fgets(nombres, 100, empleadosFile);
+			strcpy(empleados[i], strcat(nombre, ".txt"));
+			puts(nombres);
+			i++;
+		}
+	}
+	fclose(empleadosFile);
+}
+
+void leerNota(FILE *nota)
+{
 	char caracter;
 	nota = fopen("nota.txt", "r");
 
@@ -52,4 +95,14 @@ void leerNota()
 		}
 	}
 	fclose(nota);
+}
+
+void crearNotas(char **empleados, FILE *nota, int noEmpleados){
+	int tam = noEmpleados, i = 0;
+	nota = fopen("nota.txt", "r");
+	for ( i=0 ; i < tam; i++)
+	{
+		FILE *notaFinal = fopen(empleados[i], "wt");
+	}
+	
 }
